bd04e3f11ffcf01ca66c0f197d533c37adc97ffd
DAFA RPR <61611130+dafarpr@users.noreply.github.com>
2023-05-25 08:18:43 +0700

    Update

--- src/script.js
+++ src/script.js
@@ -30,3 +30,36 @@ function KelilingPP(){
 
     document.getElementById("hasil-kelilingPP").innerHTML = "K = 2 x (P + L) <br> K = 2 x (" + p + " + " + l + ") <br> K = " + w;
 }
+
+//reset
+function resetLuas(){
+    document.getElementById("sisi-luas").value = "";
+    document.getElementById("hasil-luas").innerHTML = "";
+}
+
+function resetKeliling(){
+    document.getElementById("sisi-keliling").value = "";
+    document.getElementById("hasil-keliling").innerHTML = "";
+}
+function resetLuasPP(){
+    document.getElementById("panjang-luas").value = "";
+    document.getElementById("lebar-luas").value = "";
+
+    document.getElementById("hasil-luasPP").innerHTML = "";
+}
+
+function resetKelilingPP(){
+    document.getElementById("panjang-keliling").value = "";
+    document.getElementById("lebar-keliling").value = "";
+
+    document.getElementById("hasil-kelilingPP").innerHTML = "";
+}
+//cek input
+function checkInput(){
+    var s = document.getElementById("sisi-luas").value;
+
+    if(s == ""){
+        alert("Tidak Boleh Kosong");
+        return false;
+    }
+}
